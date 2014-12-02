e1f8e56067f2275e7cde5b184d76c4d84312f90a
Nguyen The Vinh <VinhNhon@Nguyens-Mac-mini-2.local>
2014-12-02 09:53:33 +0700

    Initial Commit

---
+++ HelloWorldJamesonQuave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldJamesonQuave
+//
+//  Created by Nguyen The Vinh on 12/2/14.
+//  Copyright (c) 2014 VinhNhon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
